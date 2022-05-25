d9b86888d4257234274a74e2cc921fc2887a8ab3
ijeoma onwudiwe <onwudiweijeoma6@gmail.com>
2022-05-25 13:38:03 +0100

    here is the clean code you can read

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,45 +1,47 @@
-import React, { useState } from 'react'
-import Form from './form';
-
-
+import React, { useState } from "react";
+import Form from "./form";
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
-    const [edit, setEdit] = useState({ id: null, value: '' });
+  const [edit, setEdit] = useState({ id: null, value: "" });
 
+  // const [hidden, setHidden] = useState(false);
 
-    // const [hidden, setHidden] = useState(false);
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
 
-    const submitUpdate = (value) => {
-        updateTodo(edit.id, value);
-        setEdit({
-            id: null,
-            value: ''
-        });
-    };
+  if (edit.id) {
+    return <Form edit={edit} onSubmit={submitUpdate} />;
+  }
 
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
 
-
-    if (edit.id) {
-        return <Form edit={edit} onSubmit={submitUpdate} />;
-    }
-
-
-
-
-    return todos.map((todo, index) => (
-        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-
-            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-                {todo.text}
-            </div>
-
-            <div className='icons'>
-                <button className='editBtn' onClick={() => setEdit({ id: todo.id, value: todo.text })}> Edit </button>
-                <button className='delBtn' onClick={() => removeTodo(todo.id)}> Delete</button>
-            </div>
-        </div>
-
-    ));
-}
+      <div className="icons">
+        <button
+          className="editBtn"
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+        >
+          {" "}
+          Edit{" "}
+        </button>
+        <button className="delBtn" onClick={() => removeTodo(todo.id)}>
+          {" "}
+          Delete
+        </button>
+      </div>
+    </div>
+  ));
+};
 
 export default Todo;
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,71 +1,69 @@
-import React, { useState } from 'react'
-import Form from './form';
-import Todo from './todo';
+import React, { useState } from "react";
+import Form from "./form";
+import Todo from "./todo";
 
 const Todolist = () => {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-    /// =======ADD TODO FUNCTION
-    //========add a new item to the list
+  /// =======ADD TODO FUNCTION
+  //========add a new item to the list
 
-    const addTodo = todo => {
-        if (!todo.text || /^\s*$/.test(todo.text)) { //this line makes sure we do not type empty todo 
-            return;
-        }
-        const newTodos = [todo, ...todos];
-
-        setTodos(newTodos);
-
-
-        window.localStorage.setItem(todo.id, JSON.stringify(todo));
-        // console.log(newTodos);
-    };
-
-
-    //============edit buton function
-    const updateTodo = (todoId, newValue) => {
-
-        if (!newValue.text || /^\s*$/.test(newValue.text)) {
-            return;
-        }
-        //update the todo state
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item))
-        );
-
-        //const Edit = JSON.parse(window.localStorage.getItem(todoId));
-        window.localStorage.setItem(todoId, JSON.stringify(newValue));
-        console.log(todoId);
-    };
-
-
-    //================delete function
-    //checks the actual array and removes it from the applicationj
-    const removeTodo = (id) => {
-        const removeArr = [...todos].filter(todo => todo.id !== id);
-        setTodos(removeArr);
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      //this line makes sure we do not type empty todo
+      return;
     }
-    //output todolist function to dom
-    const completeTodo = (id) => {
-        let updatedTodos = todos.map(todo => {
+    const newTodos = [todo, ...todos];
 
-            if (todo.id === id) {
-                todo.isComplete = !todo.isComplete
-            }
-            return todo;
-        });
-        setTodos(updatedTodos);
-    };
+    setTodos(newTodos);
 
+    window.localStorage.setItem(todo.id, JSON.stringify(todo));
+    // console.log(newTodos);
+  };
 
-    return (
-        <div>
-            <Form onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
+  //============edit buton function
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    //update the todo state
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
 
+    //const Edit = JSON.parse(window.localStorage.getItem(todoId));
+    window.localStorage.setItem(todoId, JSON.stringify(newValue));
+    console.log(todoId);
+  };
 
+  //================delete function
+  //checks the actual array and removes it from the applicationj
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+  //output todolist function to dom
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
-        </div>
-    )
-}
+  return (
+    <div>
+      <Form onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
+};
 
 export default Todolist;
