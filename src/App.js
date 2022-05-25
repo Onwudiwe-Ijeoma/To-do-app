8c93ee5bc0ed73335ffa0f43842306d1249853fa
ijeoma onwudiwe <onwudiweijeoma6@gmail.com>
2022-05-25 13:45:41 +0100

    a cleaner code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import Todolist from './components/todolist';
-
+import React from "react";
+import Todolist from "./components/todolist";
 
 function App() {
-
   return (
     <div className="todo-app">
       <h1>ORGANIZE YOUR DAY</h1>
 
       <Todolist />
-
-
     </div>
   );
 }
